d97af411194cd973870142b4fcbea1e127f49b35
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-17 00:17:02 -0700

    Implement lazy mapreduce

--- src/ReduceDims.jl
+++ src/ReduceDims.jl
@@ -1,10 +1,20 @@
 module ReduceDims
 
+using Base: mapreducedim!, reduce_empty, reducedim_initarray, reducedim_init
+
 export on
 
 struct OnDims{D <: Tuple, T}
     dims::D
     x::T
+end
+
+struct MapReduce{D <: Tuple, F, B, T, K <: NamedTuple}
+    dims::D
+    f::F
+    op::B
+    x::T
+    kwargs::K
 end
 
 const ArrayLike{N} = Union{
@@ -23,9 +33,36 @@ dims(axes::Tuple{Vararg{Union{typeof(*), typeof(:)}}}) =
 on(x::ArrayLike{N}, axes::Vararg{Union{typeof(*), typeof(:)}, N}) where N =
     OnDims(dims(axes), x)
 
-@inline Base.mapreduce(f, op, x::OnDims; dims::Nothing=nothing, kwargs...) =
-    OnDims(x.dims, mapreduce(f, op, x.x; dims=x.dims, kwargs...))
+Base.mapreduce(f, op, x::OnDims; dims::Nothing=nothing, kwargs...) =
+    MapReduce(x.dims, f, op, x.x, kwargs.data)
 
+Base.dropdims(x::MapReduce) = dropdims(copy(x); dims=x.dims)
 Base.dropdims(x::OnDims) = dropdims(x.x; dims=x.dims)
+
+Base.similar(x::MapReduce) =
+    if haskey(x.kwargs, :init)
+        reducedim_initarray(x.x, x.dims, x.kwargs.init)
+    else
+        reducedim_init(x.f, x.op, x.x, x.dims)
+    end
+# TODO: don't do extra work in `similar`
+
+Base.copy(x::MapReduce) = copyto!(similar(x), x)
+
+fillinit!(dest, x) =
+    if haskey(x.kwargs, :init)
+        fill!(dest, x.kwargs.init)
+    else
+        fill!(dest, reduce_empty(x.op, eltype(dest)))
+    end
+
+Base.copyto!(dest::AbstractArray, x::MapReduce) =
+    mapreducedim!(x.f, x.op, fillinit!(dest, x), x.x)
+
+Broadcast.broadcasted(::typeof(identity), x::MapReduce) = x
+Broadcast.materialize!(dest, x::MapReduce) = copyto!(dest, x)
+
+# Support `mean(::MapReduce)` etc.
+# Broadcast.broadcastable(x::MapReduce) = copy(x)
 
 end # module
