6f4894942570b31d0e7c0a9a42c0b7e4b796ecbc
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-16 19:48:43 -0700

    Add `on` API and implement eager mapreduce

--- src/ReduceDims.jl
+++ src/ReduceDims.jl
@@ -1,5 +1,31 @@
 module ReduceDims
 
-greet() = print("Hello World!")
+export on
+
+struct OnDims{D <: Tuple, T}
+    dims::D
+    x::T
+end
+
+const ArrayLike{N} = Union{
+    AbstractArray{<:Any, N},
+    Broadcast.Broadcasted{<:Any, <:NTuple{N}},
+}
+
+@inline foldlargs(op, x) = x
+@inline foldlargs(op, x1, x2, xs...) = foldlargs(op, op(x1, x2), xs...)
+
+dims(axes::Tuple{Vararg{Union{typeof(*), typeof(:)}}}) =
+    foldlargs((1, ()), axes...) do (n, dims), ax
+        (n + 1, ax === (*) ? (dims..., n) : dims)
+    end[2]
+
+on(x::ArrayLike{N}, axes::Vararg{Union{typeof(*), typeof(:)}, N}) where N =
+    OnDims(dims(axes), x)
+
+@inline Base.mapreduce(f, op, x::OnDims; dims::Nothing=nothing, kwargs...) =
+    OnDims(x.dims, mapreduce(f, op, x.x; dims=x.dims, kwargs...))
+
+Base.dropdims(x::OnDims) = dropdims(x.x; dims=x.dims)
 
 end # module
