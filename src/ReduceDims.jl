c2af5149a123b04ab6d801128e913ce61b03d0d1
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-17 00:33:05 -0700

    Make it work with `mean`

--- src/ReduceDims.jl
+++ src/ReduceDims.jl
@@ -4,23 +4,58 @@ using Base: mapreducedim!, reduce_empty, reducedim_initarray, reducedim_init
 
 export along
 
-struct OnDims{D <: Tuple, T}
-    dims::D
-    x::T
-end
-
-struct MapReduce{D <: Tuple, F, B, T, K <: NamedTuple}
-    dims::D
-    f::F
-    op::B
-    x::T
-    kwargs::K
-end
-
 const ArrayLike{N} = Union{
     AbstractArray{<:Any, N},
     Broadcast.Broadcasted{<:Any, <:NTuple{N}},
 }
+
+abstract type Axed{T, N} <: AbstractArray{T, N} end
+# TODO: better name
+
+Base.size(x::Axed) = size(x.x)
+
+struct OnDims{T, N, D <: Tuple, X <: ArrayLike{N}} <: Axed{T, N}
+    dims::D
+    x::X
+
+    function OnDims{T, N, D, X}(dims::D, x::X) where {T, N, D <: Tuple, X <: ArrayLike{N}}
+        @assert length(dims) <= N
+        return new{T, N, D, X}(dims, x)
+    end
+end
+
+OnDims(dims, x) =
+    OnDims{eltype(x), ndims(x), typeof(dims), typeof(x)}(dims, x)
+
+Base.getindex(x::OnDims, I...) = x.x[I...]
+
+struct MapReduce{
+    N,
+    D <: Tuple,
+    F,
+    B,
+    X <: ArrayLike{N},
+    K <: NamedTuple,
+} <: Axed{Any, N}
+
+    dims::D
+    f::F
+    op::B
+    x::X
+    kwargs::K
+
+    function MapReduce{N, D, F, B, X, K}(dims::D, f::F, op::B, x::X, kwargs::K) where {N, D, F, B, X, K}
+        @assert length(dims) <= N
+        return new{N, D, F, B, X, K}(dims, f, op, x, kwargs)
+    end
+end
+
+MapReduce(dims::D, f::F, op::B, x::X, kwargs::K) where {D, F, B, X, K} =
+    MapReduce{ndims(x), D, F, B, X, K}(dims, f, op, x, kwargs)
+
+# TODO: improve
+Base.show(io::IO, x::MapReduce) = print(io, "MapReduce(...)")
+Base.show(io::IO, ::MIME"text/plain", x::MapReduce) = show(io, x)
 
 @inline foldlargs(op, x) = x
 @inline foldlargs(op, x1, x2, xs...) = foldlargs(op, op(x1, x2), xs...)
@@ -62,7 +97,7 @@ Base.copyto!(dest::AbstractArray, x::MapReduce) =
 Broadcast.broadcasted(::typeof(identity), x::MapReduce) = x
 Broadcast.materialize!(dest, x::MapReduce) = copyto!(dest, x)
 
-# Support `mean(::MapReduce)` etc.
-# Broadcast.broadcastable(x::MapReduce) = copy(x)
+# Support `mean(::OnDims)` etc.
+Broadcast.broadcastable(x::MapReduce) = copy(x)
 
 end # module
