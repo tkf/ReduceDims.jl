fbf01c3e311acc5c72167dc47403af2d2c537399
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-17 19:07:55 -0700

    Add syntax for unknown ndims

--- src/ReduceDims.jl
+++ src/ReduceDims.jl
@@ -29,6 +29,9 @@ OnDims(dims, x) =
 
 Base.getindex(x::OnDims, I...) = x.x[I...]
 
+showapply(io, x::OnDims) =
+    print(io, "along(", "::", typeof(x.x), ", ", x.dims, ")")
+
 struct MapReduce{
     N,
     D <: Tuple,
@@ -54,19 +57,70 @@ MapReduce(dims::D, f::F, op::B, x::X, kwargs::K) where {D, F, B, X, K} =
     MapReduce{ndims(x), D, F, B, X, K}(dims, f, op, x, kwargs)
 
 # TODO: improve
-Base.show(io::IO, x::MapReduce) = print(io, "MapReduce(...)")
+function Base.show(io::IO, x::MapReduce)
+    print(io, "mapreduce(", x.f, ", ", x.op, ", ")
+    showapply(io, OnDims(x.dims, x.x))
+    if !isempty(x.kwargs)
+        print(io, "; ...")
+    end
+    print(io, ")")
+end
 Base.show(io::IO, ::MIME"text/plain", x::MapReduce) = show(io, x)
 
 @inline foldlargs(op, x) = x
 @inline foldlargs(op, x1, x2, xs...) = foldlargs(op, op(x1, x2), xs...)
 
-asdims(axes::Tuple{Vararg{Union{typeof(*), typeof(:)}}}) =
+countisa(T, xs) =
+    foldlargs(0, xs...) do n, x
+        x isa T ? n + 1 : n
+    end
+
+const OpMarker = typeof(&)
+const OneOrMore = typeof(+)
+const SpecType = Union{typeof(:), OpMarker, OneOrMore}
+
+@inline function asdims(N, axes::Tuple{Vararg{SpecType}})
+    N < length(axes) && error("Too many axes specifiers.")
+    isempty(axes) && return ()
+    axes[1] isa OneOrMore && error("First axes specifier cannot be `+`.")
+
+    oom = countisa(OneOrMore, axes)
+    if oom > 1
+        error("More than one `+` is specified.")
+    elseif oom === 0
+        error("Number of axes specifier does not match with ndim.\n",
+              "Note: Use `+` to avoid repeating `:` or `&`.")
+    end
+
+    pre, post = foldlargs(((), (), Val(false)), axes...) do (pre, post, seen), ax
+        if seen === Val(true)
+            (pre, (post..., ax), Val(true))
+        elseif ax isa OneOrMore
+            (pre, post, Val(true))
+        else
+            ((pre..., ax), post, Val(false))
+        end
+    end
+    predims = asdims(pre)
+    postdims = asdims(post) .+ (N - length(post))
+    if last(pre) === (:)
+        middims = ()
+    else
+        middims = ntuple(x -> x + length(pre), N - (length(pre) + length(post)))
+    end
+    return (predims..., middims..., postdims...)
+end
+
+asdims(axes::Tuple{Vararg{Union{typeof(:), OpMarker}}}) =
     foldlargs((1, ()), axes...) do (n, dims), ax
-        (n + 1, ax === (*) ? (dims..., n) : dims)
+        (n + 1, ax isa OpMarker ? (dims..., n) : dims)
     end[2]
 
-along(x::ArrayLike{N}, axes::Vararg{Union{typeof(*), typeof(:)}, N}) where N =
+along(x::ArrayLike{N}, axes::Vararg{Union{typeof(:), OpMarker}, N}) where N =
     OnDims(asdims(axes), x)
+
+along(x::ArrayLike{N}, axes::SpecType...) where N =
+    OnDims(asdims(N, axes), x)
 
 Base.mapreduce(f, op, x::OnDims; dims::Nothing=nothing, kwargs...) =
     MapReduce(x.dims, f, op, x.x, kwargs.data)
